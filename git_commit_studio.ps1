3a52efc8e1d17905c4447443761484e9ef59ced7
JoePalek <joepalek@gmail.com>
2026-03-31 10:16:23 -0500

    chore: remove temp commit helper scripts

---
+++ git_commit_studio.ps1
@@ -0,0 +1,5 @@
+Set-Location "G:\My Drive\Projects\_studio"
+git add git_commit_studio.ps1 fix_rollup_xml.ps1 git_commit_jobmatch.ps1
+git commit -m "chore: remove temp commit helper scripts"
+git push origin master
+Write-Host "cleanup pushed"
